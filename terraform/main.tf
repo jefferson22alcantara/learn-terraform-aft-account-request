4741ca3b11ea16ef331ed1c4f14835c002203ab8
jefferson de alcantara moura <jefferson22alcantara@gmail.com>
2022-03-06 23:42:32 -0300

    new-account

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,33 @@ module "develop" {
 
   account_customizations_name = "homolog"
 }
+
+
+module "homolog-02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jefferson22alcantara@gmail.com"
+    AccountName               = "homolog-02-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "jefferson22alcantara@gmail.com"
+    SSOUserFirstName          = "homolog-02-aft"
+    SSOUserLastName           = "AFT"
+
+
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "homolog-02"
+}
